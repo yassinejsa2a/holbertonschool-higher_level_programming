0014e9c7dcaf4e194cc995f445c640d8d0ba79b4
yassinejsa21 <9872@holbertonstudents.com>
2025-02-02 22:17:32 +0100

    1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Module that defines a Square classe Module that defines a Square classe""""
+"""Module that defines a Square classe"""
 
 
 class Square:
-    """Class that defines a square Class that defines a square"""
+    """Class that defines a square"""
     def __init__(self, size):
-        """Initializes the data."""
+        """Initialize square with given size
+        Args:
+            size: size of the square
+        """
         self.__size = size
