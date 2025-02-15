3be9c3803258ee38d70ff60dc557e9adfacd8d4a
yassinejsa21 <9872@holbertonstudents.com>
2025-02-15 23:40:43 +0100

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""Write a string to a text file (UTF8)"""
+
 def write_file(filename="", text=""):
     """Writes a string to a text file"""
     with open(filename, "w", encoding="utf-8") as f:
