34c1f5ca3285fb37e198370330988f9819cf6a60
yassinejsa21 <9872@holbertonstudents.com>
2025-03-24 07:14:39 +0100

    debugging task 2 and 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,7 +10,7 @@ class Square:
             size: size of the square
         """
         self.__size = size
-	if type(size) is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
