be1cca95f475d0c35e8b72ff66a738ff7db3a97e
yassinejsa2a <9872@holbertonstudents.com>
2025-05-10 18:37:31 +0000

    Task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
 
-import sys
-
-sys.path.append('/tmp')
-
-import hidden_4
-
-for name in dir(hidden_4):
-    if not name.startswith("__"):
-        print(name)
+    for name in sorted(dir(hidden_4)):
+        if not name.startswith("__"):
+            print(name)
