eaba2aa8ac0c876da2261ee372fdde9def7b3de3
yassinejsa2a <9872@holbertonstudents.com>
2025-05-21 15:19:16 +0200

    feat: add shebang lines to new Python files in python-more_classes directory

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class.
+The Rectangle class has two attributes: width and height.
+It also has a method to calculate the area of the rectangle.
+"""
+class Rectangle:
+    """
+    A class that defines a rectangle by its width and height.
+    """
+    pass
---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class.
+The Rectangle class has two attributes: width and height.
+It also has a method to calculate the area of the rectangle.
+"""
+class Rectangle:
+    """
+    A class that defines a rectangle by its width and height.
+    """
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a new Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the rectangle."""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+        
