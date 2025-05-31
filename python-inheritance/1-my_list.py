5057ebe25ba20a5b7f35fcccc306ee3fac8d6494
yassinejsa2a <9872@holbertonstudents.com>
2025-05-31 16:00:38 +0200

    feat: update MyList class documentation and enhance test cases for print_sorted method

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
+"""
+1-my_list
+
+in this module we have the class MyList
+contains print_sorted method
+"""
 
 
-class Mylist(list):
-    """A class that inherits ."""
+class MyList(list):
+    """
+    this is a class for a create square
+    and have print_sorted method to print it
+    """
 
     def print_sorted(self):
-        """Prints the list in sorted order."""
+        """
+        that sort the actual list and
+        print the list given
+        """
         print(sorted(self))
