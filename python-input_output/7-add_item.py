60c37936babf98c8d6b818fcdd8750ef9039fc69
yassinejsa2a <9872@holbertonstudents.com>
2025-06-03 01:28:49 +0200

    feat: implement add_item function for JSON file management and add Student class with JSON serialization

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,5 +7,12 @@ from 6-load_from_json_file.py import load_from_json_file
 
 
 filename = "add_item.json"
+def add_item(item):
+    """Add an item to a JSON file."""
+    try:
+        items = load_from_json_file(filename)
+    except FileNotFoundError:
+        items = []
 
-
+    items.append(item)
+    save_to_json_file(items, filename)
---
+++ python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for student"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns the dictionary representation of a Student instance"""
+        return self.__dict__
