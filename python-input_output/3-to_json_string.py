eb5534823d5208ca5f96c507b6eb0959a489b6cc
yassinejsa2a <9872@holbertonstudents.com>
2025-06-04 14:47:17 +0200

    refactor: remove redundant import statements for json in file I/O scripts

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+import json
 
 
 def to_json_string(my_obj):
     """Returns the JSON representation of an object (string).
     """
-    import json
     return json.dumps(my_obj)
--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+import json
 
 
 def from_json_string(my_str):
     """Returns an object (Python data structure) represented by a JSON string.
     """
-    import json
     return json.loads(my_str)
