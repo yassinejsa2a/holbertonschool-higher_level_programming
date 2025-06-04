a9a3704ee82580b65c303cbd6e9a0b612cfb5317
yassinejsa2a <9872@holbertonstudents.com>
2025-06-04 15:01:15 +0200

    refactor: remove unused import statement and simplify class_to_json function

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Module for reading and printing a UTF-8 text file."""
-import json
 
 
 def class_to_json(obj):
     """Return the dictionary"""
-    return obj.__dict__ if hasattr(obj, "__dict__") else obj
+    return obj.__dict__
